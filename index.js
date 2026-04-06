3163a439e4ad9fddb50b4e4322024d6d53554c4f
sephy314 <rdy00328@gmail.com>
2026-04-06 22:39:35 +0900

    add essential things

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const net = require('net');
+const { spawn, exec } = require('child_process');
+const { Client, GatewayIntentBits, PermissionsBitField } = require('discord.js');
+
+
+function checkServer(host, port = 25565) {
+  return new Promise(resolve => {
+    const socket = new net.Socket();
+
+    socket.setTimeout(3000);
+
+    socket.on('connect', () => {
+      socket.destroy();
+      resolve(true); // 서버 켜짐
+    });
+
+    socket.on('error', () => {
+      resolve(false); // 꺼짐
+    });
+
+    socket.on('timeout', () => {
+      resolve(false);
+    });
+
+    socket.connect(port, host);
+  });
+}
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
@@ -9,11 +35,77 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+
+let serverProcess = null;
+
+function startServer() {
+  if (serverProcess) return false;
+
+  serverProcess = spawn('/home/sephy314/daechung/start', [], {
+    shell: true
+  });
+
+  serverProcess.stdout.on('data', data => {
+    console.log(`[OUT]: ${data}`);
+  });
+
+  serverProcess.stderr.on('data', data => {
+    console.error(`[ERR]: ${data}`);
+  });
+
+  serverProcess.on('close', () => {
+    console.log('프로세스 종료됨');
+    serverProcess = null;
+  });
+
+  return true;
+}
+
+
+function mcrcon(cmd) {
+  exec(`mcrcon -H 127.0.0.1 -P 10000 -p ${process.env.RCON_PASSWORD} "${cmd}"`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error('에러:', err);
+        return;
+      }
+      console.log(stdout);
+    }
+  );
+}
+
+client.on('messageCreate', async message => {
   if (message.author.bot) return;
 
-  if (message.content === '$ping') {
-    message.reply('pong 🏓');
+  switch (message.content) {
+    case '$ping':
+      message.reply('pong 🏓');
+      break;
+
+    case '$마크켜':
+      if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+        message.reply("권한이 없어요!");
+        break;
+      }
+      const sv = startServer();
+      if (sv) message.reply("켜짐");
+      break;
+
+    case "$마크꺼":
+      if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+        message.reply("권한이 없어요!");
+        break;
+      }
+      mcrcon('stop');
+      message.reply("끔");
+      break;
+
+    case "$마크상태":
+      const online = await checkServer();
+      if (online) message.reply("서버 켜짐")
+      else message.reply("꺼짐")
+
+      break;
   }
 });
 
